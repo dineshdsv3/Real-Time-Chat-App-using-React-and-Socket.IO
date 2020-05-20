2f8a73ef859d1b60723959f9c5bff282d8b98542
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-21 00:04:53 +0530

    Initialized socket manager.Io

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+const SocketManager = require('./SocketManager') 
 
 const PORT = process.env.port || 3231;
 
-
+io.on('connection',SocketManager);
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
+
+
+module.exports = io;
