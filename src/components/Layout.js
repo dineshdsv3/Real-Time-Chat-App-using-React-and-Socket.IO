739c912d1191f97dba436825d835660e4eb0d1b7
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-21 07:12:30 +0530

    Changed socket configuration -- Working code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import io from 'socket.io-client';
 
+const socketUrl = 'http://192.168.0.118:3231';
 class Layout extends React.Component {
+
+    state = {
+        socket: 'null'
+    }
+
+    componentDidMount() {
+        this.initSocket();
+    }
+
+    initSocket = () => {
+        const socket = io(socketUrl);
+        socket.on('connect', () => {
+            console.log('Connected')
+        })
+        this.setState({socket});
+    }
+
     render() {
         const {title} = this.props;
         return (
--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
-var server = require('http').createServer(app);
+const http = require('http');
+var server = http.createServer(app);
 var io = require('socket.io')(server);
 
-const SocketManager = require('./SocketManager') 
+const SocketManager = require('./SocketManager');
 
 const PORT = process.env.port || 3231;
 
-io.on('connection',SocketManager);
+io.on('connection', SocketManager);
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
-
+server.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
+});
 
 module.exports = io;
