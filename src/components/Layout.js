d3a0512ee272ac99992fdc00382402d93bb19cc5
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-21 07:42:35 +0530

    Add User function added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import io from 'socket.io-client';
+import { USER_CONNECTED } from '../Events';
 
 const socketUrl = 'http://192.168.0.118:3231';
 class Layout extends React.Component {
+	state = {
+		socket: 'null',
+		user: 'null',
+	};
 
-    state = {
-        socket: 'null'
-    }
+	componentDidMount() {
+		this.initSocket();
+	}
 
-    componentDidMount() {
-        this.initSocket();
-    }
+	initSocket = () => {
+		const socket = io(socketUrl);
+		socket.on('connect', () => {
+			console.log('Connected');
+		});
+		this.setState({ socket });
+	};
 
-    initSocket = () => {
-        const socket = io(socketUrl);
-        socket.on('connect', () => {
-            console.log('Connected')
-        })
-        this.setState({socket});
-    }
+	setUser = (user) => {
+		const { socket } = this.state;
+		socket.emit(USER_CONNECTED, user);
+		this.setState({ user });
+	};
 
-    render() {
-        const {title} = this.props;
-        return (
-            <div className="container">
-                {title}
-            </div>
-        )
-    }
+	render() {
+		const { title } = this.props;
+		return <div className="container">{title}</div>;
+	}
 }
 
 export default Layout;
