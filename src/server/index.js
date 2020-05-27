2233af0ca1570df51ab037b78f1461fb5af248e8
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-27 18:11:54 +0530

    change in index for heroku

--- src/server/index.js
+++ src/server/index.js
@@ -6,12 +6,12 @@ var io =  module.exports.io = require('socket.io')(server);
 
 const SocketManager = require('./SocketManager');
 
-const PORT = process.env.port || 3231;
+const port = process.env.PORT;
 
 app.use(express.static(__dirname + '/../../build'))
 
 io.on('connection', SocketManager);
 
-server.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`);
+server.listen(port, () => {
+	console.log(`Server started on port ${port}`);
 });
